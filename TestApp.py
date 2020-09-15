c5ec7c2406d106c73c94b6d2dd26fd6046d50c24
katyushka02 <ebaraniuk02@gmail.com>
2020-09-15 17:28:13 +0300

    commit #2

--- TestApp.py
+++ TestApp.py
@@ -1,9 +1,18 @@
 import random
 import numpy as np
 
+
 def array():
-    a = np.array([[random.randint(0,100) for i in range(4)],[random.randint(0,100) for i in range(4)],
-                  [random.randint(0,100) for i in range(4)],[random.randint(0,100) for i in range(4)]])
-    print(a)
+    a = np.array([[random.randint(0, 10) for i in range(4)], [random.randint(0, 10) for i in range(4)],
+                  [random.randint(0, 10) for i in range(4)], [random.randint(0, 10) for i in range(4)]])
     return a
 
+
+def mult():
+    a = array()
+    b = array()
+
+    print(a, b, np.dot(a, b))
+
+
+mult()
